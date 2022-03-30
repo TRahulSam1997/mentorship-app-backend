d1b1569417ffac9f576dc9b55a84cb2c2bbfef20
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-03-30 21:48:19 +1100

    kafka consumer config added

--- src/main/java/com/mentorshipAppBackend/config/KafkaConsumerConfig.java
+++ src/main/java/com/mentorshipAppBackend/config/KafkaConsumerConfig.java
@@ -3,7 +3,12 @@ package com.mentorshipAppBackend.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,5 +25,10 @@ public class KafkaConsumerConfig {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> producerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
     }
 }
