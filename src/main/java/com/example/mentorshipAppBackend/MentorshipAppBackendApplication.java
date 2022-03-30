34adc435314419a4b50d6e9f662799d00be90067
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-03-30 11:53:24 +1100

    setting kafka properties

---
+++ src/main/java/com/example/mentorshipAppBackend/MentorshipAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentorshipAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorshipAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorshipAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mentorshipAppBackend/MentorshipAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentorshipAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorshipAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
