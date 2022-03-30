5cd00476b1d219aab147274793ec044309f386fb
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-03-30 12:13:07 +1100

    topic builder added

--- src/main/java/com/mentorshipAppBackend/config/KafkaTopicConfig.java
+++ src/main/java/com/mentorshipAppBackend/config/KafkaTopicConfig.java
@@ -2,11 +2,13 @@ package com.mentorshipAppBackend.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configurable
 public class KafkaTopicConfig {
 
+    @Bean
     public NewTopic mentorshipAppTopic() {
         return TopicBuilder.name("mentorshipApp")
                 .build();
